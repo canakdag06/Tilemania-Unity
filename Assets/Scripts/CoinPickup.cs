bf9937239929313421915c8d65b62926fb2e28f1
Can Akdag <canakdag06@gmail.com>
2025-11-04 21:35:36 +0300

    added Coin

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
